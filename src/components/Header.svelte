635e32cf6f9b3ab68fb0f2e0e3f2094974e4d30e
Alejandro Cortez <acortez.dev@gmail.com>
2020-06-05 11:28:26 -0500

    Added prefetch

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,11 @@
 <script>
+  import { goto } from '@sapper/app';
   import { likeCount } from '../store/store';
   export let segment;
+
+  const navigateHome = async () => {
+    await goto('/')
+  };
 </script>
 
 <style>
@@ -74,7 +79,7 @@
   <div class="Header-container">
     <div class="Header-content">
       <div class="Header-logo">
-        <h1>Pelusigram</h1>
+        <h1 on:click={navigateHome}>Pelusigram</h1>
       </div>
       <div class="Header-nav">
         <ul>
@@ -83,7 +88,9 @@
               {$likeCount === 0 ? '' : $likeCount}
           </li>
           <li>
-            <a aria-current={segment === 'profile' ? 'page' : undefined } href="profile">
+            <a aria-current={segment === 'profile' ? 'page' : undefined }
+               rel="prefetch"
+               href="profile">
                 <i class="fas fa-user-alt"></i>
             </a>
           </li>
